9e8edc616a701fe6133a19477f7fb7ba23b93652
Lucas Neves <lcneves@gmail.com>
2017-05-04 21:33:17 +0000

    Mocha script for testing

---
+++ test/theme-test.js
@@ -0,0 +1,24 @@
+/*
+ * theme-test.js
+ * Copyright 2017 Lucas Neves <lcneves@gmail.com>
+ *
+ * Mocha unit test for themes to work with the Livre3D engine.
+ * Part of the Livre project.
+ */
+
+const assert = require('assert'),
+      THREE = require('three'),
+      theme = require('../theme.js');
+
+describe('theme', function () {
+  it('should export a "Scene" function', function () {
+    assert(typeof theme.Scene === 'function');
+  });
+
+  describe('Scene', function () {
+    it('should create an instance of THREE.Scene', function () {
+      var scene = new theme.Scene;
+      assert(scene instanceof THREE.Scene);
+    });
+  });
+});
